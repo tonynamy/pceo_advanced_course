ed7b786991b483338cbb23e309d78ce70e70feae
huemone_jhoh <jhoh@huemone.com>
2019-07-30 14:03:52 +0900

    add_pages

--- 03_flask/app.py
+++ 03_flask/app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
   return "Hello world!"
 
+@app.route("/index")
+def index():
+  return "Index Page"
+
+@app.route("/about")
+def about():
+  return "About Page"
+
 if __name__ == '__main__':
   app.run()
